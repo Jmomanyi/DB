a2a6ad7c84a0558b201a94e816de605c5dec22e9
Jmomanyi <jjjmomanyi@gmail.com>
2022-11-04 15:50:07 +0000

    khfbrf

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$host = "silva.computing.dundee.ac.uk";
+$username = "jaredmomanyi";
+$password = "AC32006";
+$database = "jaredmomanyidb";
+$mysql = new PDO("mysql:host=".$host.";dbname=".$database,$username,$password);
+?>
